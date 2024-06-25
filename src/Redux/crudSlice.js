61af0f57b7e6771d5fb05d65127b9b7ee74071b4
Udichi Mukherjee <udichimukherjee42@gmail.com>
2024-06-26 01:56:23 +0530

    Updated

--- src/Redux/crudSlice.js
+++ src/Redux/crudSlice.js
@@ -20,40 +20,7 @@ const initialState = {
 
 };
 
-export const product = createAsyncThunk( 
-  "/student/create",
 
-  async (formData) => {
-    try{
-    let res = await axios.post(`https://tureappservar.onrender.com/student/create`, formData); 
-console.log("Response Data",res)
-    let resData = res?.data;
-    
-
-
-    return resData;
-  } catch(error){
-
-console.log("error",error)
-
-
-return error.response.data
-
-  }
-});
-
-
-export const list = createAsyncThunk(
-  "/student/show",
-
-  async (formData) => {
-    let res = await axios.get(`https://tureappservar.onrender.com/student/show`, formData);
-
-    let resData = res?.data;
-
-    return resData;
-  }
-);
 
 
 
@@ -98,30 +65,10 @@ export const remove = createAsyncThunk(
   }
 );
 
-export const update = createAsyncThunk(
-  "/student/",
-
-  async (slug) => {
-    let res = await axios.get(`/student/${slug}`, slug);
-
-    let resData = res?.data;
-
-    return resData;
-  }
-);
 
 
-export const productDetails = createAsyncThunk(
-  "/student/{slug/id}",
 
-  async (slug) => {
-    let res = await axios.get(`https://tureappservar.onrender.com/student/${slug}`, slug);
 
-    let resData = res?.data;
-
-    return resData;
-  }
-);
 
 
 export const blogUpdate = createAsyncThunk(
@@ -171,58 +118,11 @@ export const productSlice = createSlice({
   extraReducers: (builder) => {
     builder
 
-      .addCase(product.pending, (state, action) => {
-        state.status = "loading";
-        console.log("Pending State")
-      })
-      .addCase(product.fulfilled, (state, action) => {
-        state.status = "idle";
-        
-        
-          if (action.payload?.data?._id) {
-          state.err=!state.err
-          console.log("updated state",state.err)
-        toast("Success, Student added successfully!!",action.payload?.message);
-        }
-        else{
-          action.payload?.error?.name && toast(action.payload?.error?.name?.message);
-          action.payload?.error?.email && toast(action.payload?.error?.email?.message);
-          action.payload?.error?.phone && toast(action.payload?.error?.phone?.message);
-          action.payload?.error?.city && toast(action.payload?.error?.city?.message);
-          action.payload?.error?.state && toast(action.payload?.error?.state?.message);
-          action.payload?.error?.address && toast(action.payload?.error?.address?.message);
-          action.payload?.error?.section && toast(action.payload?.error?.section?.message);
-          action.payload?.error?.classes && toast(action.payload?.error?.classes?.message);
-
-          toast(action.payload?.message);
-          
-          
-        }
+      
         
 
-      })
-      .addCase(product.rejected, (state, action) => {
-        state.status = "idle";
-        
-
-        toast(action.error);
-        
-
-      }).addCase(list.pending, (state, action) => {
-        state.status = "loading";
-      }).addCase(list.fulfilled, (state, { payload }) => {
-        state.status = "idle";
-        if (payload.success === 200) {
-          state.listData = payload.data
-          state.totalRecords = payload.totalRecords;
-          console.log("LiatData",state.listData)
-          console.log("TotalRecords",state.totalRecords)
-         
-        }
-      })
-      .addCase(list.rejected, (state, action) => {
-        state.status = "idle";
-      })
+      
+     
       .addCase(create.pending, (state, action) => {
         state.status = "loading";
     })
@@ -271,42 +171,7 @@ export const productSlice = createSlice({
     .addCase(remove.rejected, (state, action) => {
         state.status = "idle";
     })
-    .addCase(update.pending, (state, action) => {
-      state.status = "loading";
-    })
-    .addCase(update.fulfilled, (state, { payload }) => {
-      state.status = "idle";
-      
-      if (payload.status === 200) {
-        state.totalRecords = payload.totalRecords;
-        toast(payload.message);
-      }
-      else{
-        toast(payload.message);
-      }
-
-      
-
-    })
-    .addCase(update.rejected, (state, action) => {
-      state.status = "idle";
-    })
-
-
-
-    .addCase(productDetails.pending, (state, action) => {
-      state.status = "loading";
-    })
-    .addCase(productDetails.fulfilled, (state, { payload }) => {
-      state.status = "idle";
-      
-        state.det = payload;
-        console.log("det",typeof(state.det.data))
-      
-    })
-    .addCase(productDetails.rejected, (state, action) => {
-      state.status = "idle";
-    })
+   
     .addCase(blogUpdate.pending, (state, action) => {
       state.status = "loading";
     })
